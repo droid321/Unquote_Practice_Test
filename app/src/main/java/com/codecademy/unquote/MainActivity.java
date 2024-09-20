c043a468b7d9efd47e05a76a12549be5821c6a76
droid321 <cgpsanagustin@yahoo.com>
2024-09-20 10:32:47 +1000

    Initial commit

---
+++ app/src/main/java/com/codecademy/unquote/MainActivity.java
@@ -0,0 +1,154 @@
+package com.codecademy.unquote;
+import com.codecademy.unquote.Question;
+import com.codecademy.unquote.R;
+
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    int currentQuestionIndex;
+    int totalCorrect;
+    int totalQuestions;
+    ArrayList<Question> questions;
+
+    // TODO 3-A: Declare View member variables
+    ImageView questionImageView = findViewById(R.id.iv_main_question_image);
+    TextView questionTextView = findViewById(R.id.tv_main_question_title);
+    TextView questionsRemainingTextView = findViewById(R.id.tv_main_questions_remaining);
+    TextView questionsRemainingCounter = findViewById(R.id.tv_main_questionNumber);
+    Button answer0Button = findViewById(R.id.btn_main_answer_0);
+    Button answer1Button = findViewById(R.id.btn_main_answer_1);
+    Button answer2Button = findViewById(R.id.btn_main_answer_2);
+    Button answer3Button = findViewById(R.id.btn_main_answer_3);
+    Button submitButton = findViewById(R.id.btn_main_submit_answer);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+
+        super.onCreate(savedInstanceState);
+
+        // TODO 2-G: Show app icon in ActionBar
+        /*getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.ic_unquote_icon);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setElevation(0);*/
+
+
+        setContentView(R.layout.activity_main);
+
+
+        // TODO 3-B: assign View member variables
+
+        // TODO 4-E: set onClickListener for each answer Button
+
+        // TODO 5-A: set onClickListener for the submit answer Button
+
+        startNewGame();
+    }
+
+
+    // TODO 3-F: displayQuestion(Question question) {...}
+
+    // TODO 3-C:
+      void displayQuestionsRemaining(int questionRemaining) {
+        questionsRemainingCounter.setText(questionRemaining);
+      }
+
+    // TODO 4-A: onAnswerSelected(int answerSelected) {...}
+
+    void onAnswerSubmission() {
+        Question currentQuestion = getCurrentQuestion();
+        if (currentQuestion.isCorrect()) {
+            totalCorrect = totalCorrect + 1;
+        }
+        questions.remove(currentQuestion);
+
+        // TODO 3-D.i: Uncomment the line below after implementing displayQuestionsRemaining(int)
+         displayQuestionsRemaining(questions.size());
+
+        if (questions.size() == 0) {
+            String gameOverMessage = getGameOverMessage(totalCorrect, totalQuestions);
+
+            // TODO 5-D: Show a popup instead
+            System.out.println(gameOverMessage);
+        } else {
+            chooseNewQuestion();
+
+            // TODO 3-H.i: uncomment after implementing displayQuestion(Question)
+            //displayQuestion(getCurrentQuestion());
+        }
+    }
+
+    void startNewGame() {
+        questions = new ArrayList<>();
+
+
+        // TODO 2-H: Provide actual drawables for each of these questions!
+        Question question0 = new Question(R.drawable.img_quote_0, "Pretty good advice, and perhaps a scientist did say it... Who actually did?", "Albert Einstein", "Simon Nguyen", "Rita Mae Brown", "Rosalind Franklin", 2);
+        Question question1 = new Question(R.drawable.img_quote_1, "Was honest Abe honestly quoted? Who authored this pithy bit of wisdom?", "Edward Stieglitz", "Maya Angelou", "Abraham Lincoln", "Ralph Waldo Emerson", 0);
+        Question question2 = new Question(R.drawable.img_quote_2, "Easy advice to read, difficult advice to follow - Who said it?", "Martin Luther King Jr.", "Mother Teresa", "Simon Nguyen", "Zane", 1);
+        Question question3 = new Question(R.drawable.img_quote_3, "Insanely inspiring, insanely incorrect(maybe). Who is the true source of this inspiration?", "Nelson Mandela", "Harriet Tubman", "Mahatma Gandhi", "Nicholas Klein", 3);
+        Question question4 = new Question(R.drawable.img_quote_4, "A peace worth striving for - who actually reminded us of this?", "Malala Yousafzai", "Martin Luther King Jr.", "Liu Xiaobo", "Dalai Lama", 1);
+        Question question5 = new Question(R.drawable.img_quote_5, "Unfortunately, true - but did Marilyn Monroe convey it or did someone else?", "Laurel Thatcher Ulrich", "Marilyn Monroe", "Eleanor Roosevelt", "Queen Victoria", 0);
+
+        questions.add(question0);
+        questions.add(question1);
+        questions.add(question2);
+        questions.add(question3);
+        questions.add(question4);
+        questions.add(question5);
+
+        totalCorrect = 0;
+        totalQuestions = questions.size();
+
+        Question firstQuestion = chooseNewQuestion();
+
+        // TODO 3-D.ii: Uncomment the line below after implementing displayQuestionsRemaining(int)
+          displayQuestionsRemaining(questions.size());
+
+        // TODO 3-H.ii: Uncomment after implementing displayQuestion(Question)
+        // displayQuestion(firstQuestion);
+    }
+
+    Question chooseNewQuestion() {
+        int newQuestionIndex = generateRandomNumber(questions.size());
+        currentQuestionIndex = newQuestionIndex;
+        return questions.get(currentQuestionIndex);
+    }
+
+    int generateRandomNumber(int max) {
+        double randomNumber = Math.random();
+        double result = max * randomNumber;
+        return (int) result;
+    }
+
+    Question getCurrentQuestion() {
+        Question currentQuestion = questions.get(currentQuestionIndex);
+        return currentQuestion;
+    }
+
+    String getGameOverMessage(int totalCorrect, int totalQuestions) {
+        if (totalCorrect == totalQuestions) {
+            return "You got all " + totalQuestions + " right! You won!";
+        } else {
+            return "You got " + totalCorrect + " right out of " + totalQuestions + ". Better luck next time!";
+        }
+    }
+}
